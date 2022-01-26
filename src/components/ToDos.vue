95e02a77503ed2b55c084192d92f592dea1dc7c7
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-01-26 14:33:53 +0100

    Tutorial gefolgt

---
+++ src/components/ToDos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ToDoItem  v-for="todo in todoEntries" v-bind:id="todo.id" v-bind:key="todo.id"  v-bind:todo_prop="todo"  @delete-todo-event="$emit('delete-todo-event', todo.id)"  />
+    </div>
+</template>
+<script>
+    console.log("todos exec")
+    import ToDoItem from "./ToDoItem";
+    export default {
+        name: 'ToDos',
+        components: {
+            ToDoItem
+        },
+        props: [
+            "todoEntries"
+        ]
+    }
+    console.log("todos end")
+</script>
