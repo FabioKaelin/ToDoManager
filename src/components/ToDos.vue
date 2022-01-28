86a0fd12f19c154da1ad3cfb67eeabb5fb3f17bd
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-01-28 14:58:32 +0100

    Funktioniert

--- src/components/ToDos.vue
+++ src/components/ToDos.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="TaskListe">
-        <ToDoItem  v-for="todo in todoEntries" v-bind:id="todo.id" v-bind:key="todo.id"  v-bind:todo_prop="todo"  @delete-todo-event="$emit('delete-todo-event', todo.id)" @edit-todo-event="$emit('edit-todo-event', todo.id)" />
+        <ToDoItem  v-for="todo in todoEntries" v-bind:id="todo.id" v-bind:key="todo.id"  v-bind:todo_prop="todo"  @delete-todo-event="$emit('delete-todo-event', todo.id)" @change-Edit-Input="$emit('change-Edit-Input', todo.id)" @edit-todo-event="$emit('edit-todo-event', todo.id)" />
     </div>
         <svg height="100" width="100">
                 <polyline points="14,72 74,8 74.5,10 75,11 78,12 79,12 19,78 11,81 14.3,72.2 19,78"
