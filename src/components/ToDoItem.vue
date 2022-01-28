d98651238e0be25264c53e8b8a45902632bf5a3c
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-01-28 14:57:07 +0100

    Funktioniert

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="todoitem"  @click="markCompleted" v-bind:class="{'completed':this.todo.completed}">
-        <span class="Taskname">{{todo.title}}</span>
-        <input type="text" @blur="$emit('edit-todo-event', todo.id)"  class="editInput">
+    <div class="todoitem" v-bind:class="{'completed':this.todo.completed}">
+        <span @click="markCompleted"  class="Taskname">{{todo.title}}</span>
+        <!-- @blur="$emit('edit-todo-event', todo.id)" -->
+        <input type="text"  @keydown="$emit('edit-todo-event', todo.id)" class="editInput">
+        <input type="button" class="showInputButton" value="edit"  @click="$emit('change-Edit-Input', todo.id)">
         <!-- @blur="editToDoItem" -->
 
         <button class="papierkorbButton" @click="$emit('delete-todo-event', todo.id)">
@@ -14,16 +16,7 @@
                 style="fill:none;stroke:black;stroke-width:1.5" />
             </svg>
         </button>
-        <button class="editButton" @click="$emit('edit-todo-event', todo.id)">
-            <svg height="20" width="20">
-                <polyline points="5,7.5 5,15 7.5,17.5 12.5,17.5 15,15 15,7.5"
-                style="fill:none;stroke:black;stroke-width:1.5" />
-                <polyline points="5,5 15,5 16,5.5 16.5,6.125 16,7 15,7.5 5,7.5 4,7 3.5,6.125 4,5.5 5,5 15,5"
-                style="fill:none;stroke:black;stroke-width:1.5" />
-                <polyline points="7.5,5 8.5625,3.0625  10,2.5 11.4375,3.0625 12.5,5"
-                style="fill:none;stroke:black;stroke-width:1.5" />
-            </svg>
-        </button>
+        <hr class="ToDoTrennlinie" noshade>
             <!-- <svg height="40" width="40">
                 <polyline points="10,15 10,30 15,35 25,35 30,30 30,15"
                 style="fill:none;stroke:black;stroke-width:3" />
@@ -113,7 +106,26 @@
             },
             editToDoItem(toDoId){
 				console.log(this.todoEntries[toDoId - 1].title)
-			}
+			},
+            showEditInput(toDoId){
+                console.log("hallo")
+                let id;
+                console.log(this.todo_prop)
+				for (const element of this.todo_prop) {
+                    console.log(element)
+					if(toDoId == element.id){
+						id = element.id
+                        console.log(element)
+
+					}
+				}
+                var x = document.getElementsByClassName("editInput")[id];
+                if (x.style.display === "none") {
+                    x.style.display = "unset";
+                } else {
+                    x.style.display = "none";
+                }
+            }
         }
     }
     console.log("item end")
