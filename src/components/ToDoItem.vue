9e6e0c946f3f141f774e2024824bb29cae2c7959
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-01-31 07:34:57 +0100

    Stift-SVG added

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -3,11 +3,24 @@
         <span @click="markCompleted"  class="Taskname">{{todo.title}}</span>
         <!-- @blur="$emit('edit-todo-event', todo.id)" -->
         <input type="text"  @keydown="$emit('edit-todo-event', todo.id)" class="editInput">
-        <input type="button" class="showInputButton" value="edit"  @click="$emit('change-Edit-Input', todo.id)">
+        <!-- <input type="button" class="showInputButton" value="edit"  @click="$emit('change-Edit-Input', todo.id)"> -->
         <!-- @blur="editToDoItem" -->
 
-        <button class="papierkorbButton" @click="$emit('delete-todo-event', todo.id)">
+        <button class="showInputButton" @click="$emit('change-Edit-Input', todo.id)">
             <svg height="20" width="20">
+                <polyline points="2.8,14.4 14.8,1.6 14.88,2 15,2.2 15.6,2.4 15.8,2.4 3.8,15.6 2.2,16.2 2.86,14.44 3.8,15.6"
+                style="fill:none;stroke:black;stroke-width:0.75" />
+                <polyline points="9.8,7 10.8,8 10.4,8.4 9.4,7.4"
+                style="fill:none;stroke:black;stroke-width:0.75" />
+                <polyline points="14.8,1.4 15.4,1.6 15.8,1.8 15.9,2.3"
+                style="fill:none;stroke:black;stroke-width:0.75" />
+                <polyline points="13.94,2.6 13.2,2.6 12.8,2.8 10,5.8"
+                style="fill:none;stroke:black;stroke-width:0.75" />
+            </svg>
+        </button>
+
+        <button class="papierkorbButton" @click="$emit('delete-todo-event', todo.id)">
+            <svg class="papierkorb" height="20" width="20">
                 <polyline points="5,7.5 5,15 7.5,17.5 12.5,17.5 15,15 15,7.5"
                 style="fill:none;stroke:black;stroke-width:1.5" />
                 <polyline points="5,5 15,5 16,5.5 16.5,6.125 16,7 15,7.5 5,7.5 4,7 3.5,6.125 4,5.5 5,5 15,5"
@@ -17,14 +30,6 @@
             </svg>
         </button>
         <hr class="ToDoTrennlinie" noshade>
-            <!-- <svg height="40" width="40">
-                <polyline points="10,15 10,30 15,35 25,35 30,30 30,15"
-                style="fill:none;stroke:black;stroke-width:3" />
-                <polyline points="10,10, 30,10 32,11 33,12.5 32,14 30,15 10,15 8,14 7,12.5 8,11 10,10 30,10"
-                style="fill:none;stroke:black;stroke-width:3" />
-                <polyline points="15,10 17.125,6.125  20,5 22.875,6.125 25,10"
-                style="fill:none;stroke:black;stroke-width:3" />
-            </svg> -->
     </div>
 </template>
 <script>
@@ -131,6 +136,3 @@
     console.log("item end")
 </script>
 
-<style scoped>
-	@import "../../style.css"
-</style>
